5c85c802d1c0a65beedf09e9921eb2c6fa08f5a6
MyvinB <46648232+MyvinB@users.noreply.github.com>
2020-03-20 11:21:40 +0530

    Create Panda.java
    Abstract Class Vs Interface by example

---
+++ src/Theory/Panda.java
@@ -0,0 +1,28 @@
+abstract class PandaAbstract extends Panda {
+    //implementation of abstract and non abstract methods
+    abstract String chicken();
+
+    String TandooriChicken() {
+        return null;
+    }
+}
+
+interface PandaInterface{
+    //By default all methods in the
+    String hello();
+    //all variable declared static final
+    int soup = 0;
+    //Since java 8 we have default methods allowed in interface
+    default String pandaSleep(){
+        System.out.println("Go to sleep Panda");
+        return "Done";
+    }
+    static String eatPanda(){
+        System.out.println();
+        return "Done";
+    }
+}
+
+class Panda{
+public int chicken=0;
+}
